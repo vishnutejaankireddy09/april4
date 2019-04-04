d57910705b9e341739dda496ea0562516e1af2a8
Vishnu Teja Anki Reddy Konda <741054@student.tor.loc>
2019-04-04 11:39:33 -0400

    done

---
+++ FILEE.js
@@ -0,0 +1,10 @@
+var user ={
+    first_name:"css",
+    last_name:"HTML",
+    age: 4, /*from w w  w. java  2 s   .c o m */
+    wesite:"java2s.com"
+    };
+    for(key in user){
+        
+        console.log(key);
+    }
---
+++ code.js
@@ -0,0 +1,12 @@
+    
+var a=1;
+var sum=0;
+while(a<=11){
+    document.write(a+" ");
+ sum= sum+a;
+a++;
+
+
+}
+
+document.write(sum);
